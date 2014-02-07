5f401d475b0facf2ca7f754eaa7c6bc0a014abba
Casey Ydenberg <ydenberg@gmail.com>
2014-02-07 15:46:06 -0500

    Added a circle

--- OOChart.js
+++ OOChart.js
@@ -4,7 +4,7 @@ function createDrawingObject(ctx) {
     var that = {
         xPos: 0,
         yPos: 0,
-        color: '#000000;',
+        color: 'black',
         draw: function() {
         ctx.beginPath(that.xPos, that.yPos);
             ctx.stroke();
@@ -14,13 +14,26 @@ function createDrawingObject(ctx) {
     return that;
 }
 
-function createLine(ctx) {
+function createXAxis(ctx) {
 	var that = createDrawingObject(ctx);
 	that.length = 100;
 	that.draw = function() {
 		ctx.beginPath();
+		ctx.strokeStyle = that.color;
 		ctx.moveTo(that.xPos, that.yPos);
 		ctx.lineTo(that.xPos + that.length, that.yPos);
+		ctx.stroke();
+	}
+	return that;
+}
+
+function createCircle(ctx) {
+	var that = createDrawingObject(ctx);
+	that.radius = 100;
+	that.draw = function() {
+		ctx.beginPath();
+		ctx.arc(that.xPos, that.yPos, that.radius, 0, 2 * Math.PI, 'clockwise');
+		ctx.strokeStyle = that.color;
 		ctx.stroke();
 	}
 	return that;

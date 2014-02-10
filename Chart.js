195af5f5053ea33d72c1ccb5684f9d11b77659e7
Casey Ydenberg <ydenberg@gmail.com>
2014-02-10 14:03:40 -0500

    Reordered the code base and added some comments

--- Chart.js
+++ Chart.js
@@ -12,141 +12,6 @@ window.Chart = function(context){
 
 	var chart = this;
 	
-	
-	//Easing functions adapted from Robert Penner's easing equations
-	//http://www.robertpenner.com/easing/
-	
-	var animationOptions = {
-		linear : function (t){
-			return t;
-		},
-		easeInQuad: function (t) {
-			return t*t;
-		},
-		easeOutQuad: function (t) {
-			return -1 *t*(t-2);
-		},
-		easeInOutQuad: function (t) {
-			if ((t/=1/2) < 1) return 1/2*t*t;
-			return -1/2 * ((--t)*(t-2) - 1);
-		},
-		easeInCubic: function (t) {
-			return t*t*t;
-		},
-		easeOutCubic: function (t) {
-			return 1*((t=t/1-1)*t*t + 1);
-		},
-		easeInOutCubic: function (t) {
-			if ((t/=1/2) < 1) return 1/2*t*t*t;
-			return 1/2*((t-=2)*t*t + 2);
-		},
-		easeInQuart: function (t) {
-			return t*t*t*t;
-		},
-		easeOutQuart: function (t) {
-			return -1 * ((t=t/1-1)*t*t*t - 1);
-		},
-		easeInOutQuart: function (t) {
-			if ((t/=1/2) < 1) return 1/2*t*t*t*t;
-			return -1/2 * ((t-=2)*t*t*t - 2);
-		},
-		easeInQuint: function (t) {
-			return 1*(t/=1)*t*t*t*t;
-		},
-		easeOutQuint: function (t) {
-			return 1*((t=t/1-1)*t*t*t*t + 1);
-		},
-		easeInOutQuint: function (t) {
-			if ((t/=1/2) < 1) return 1/2*t*t*t*t*t;
-			return 1/2*((t-=2)*t*t*t*t + 2);
-		},
-		easeInSine: function (t) {
-			return -1 * Math.cos(t/1 * (Math.PI/2)) + 1;
-		},
-		easeOutSine: function (t) {
-			return 1 * Math.sin(t/1 * (Math.PI/2));
-		},
-		easeInOutSine: function (t) {
-			return -1/2 * (Math.cos(Math.PI*t/1) - 1);
-		},
-		easeInExpo: function (t) {
-			return (t==0) ? 1 : 1 * Math.pow(2, 10 * (t/1 - 1));
-		},
-		easeOutExpo: function (t) {
-			return (t==1) ? 1 : 1 * (-Math.pow(2, -10 * t/1) + 1);
-		},
-		easeInOutExpo: function (t) {
-			if (t==0) return 0;
-			if (t==1) return 1;
-			if ((t/=1/2) < 1) return 1/2 * Math.pow(2, 10 * (t - 1));
-			return 1/2 * (-Math.pow(2, -10 * --t) + 2);
-			},
-		easeInCirc: function (t) {
-			if (t>=1) return t;
-			return -1 * (Math.sqrt(1 - (t/=1)*t) - 1);
-		},
-		easeOutCirc: function (t) {
-			return 1 * Math.sqrt(1 - (t=t/1-1)*t);
-		},
-		easeInOutCirc: function (t) {
-			if ((t/=1/2) < 1) return -1/2 * (Math.sqrt(1 - t*t) - 1);
-			return 1/2 * (Math.sqrt(1 - (t-=2)*t) + 1);
-		},
-		easeInElastic: function (t) {
-			var s=1.70158;var p=0;var a=1;
-			if (t==0) return 0;  if ((t/=1)==1) return 1;  if (!p) p=1*.3;
-			if (a < Math.abs(1)) { a=1; var s=p/4; }
-			else var s = p/(2*Math.PI) * Math.asin (1/a);
-			return -(a*Math.pow(2,10*(t-=1)) * Math.sin( (t*1-s)*(2*Math.PI)/p ));
-		},
-		easeOutElastic: function (t) {
-			var s=1.70158;var p=0;var a=1;
-			if (t==0) return 0;  if ((t/=1)==1) return 1;  if (!p) p=1*.3;
-			if (a < Math.abs(1)) { a=1; var s=p/4; }
-			else var s = p/(2*Math.PI) * Math.asin (1/a);
-			return a*Math.pow(2,-10*t) * Math.sin( (t*1-s)*(2*Math.PI)/p ) + 1;
-		},
-		easeInOutElastic: function (t) {
-			var s=1.70158;var p=0;var a=1;
-			if (t==0) return 0;  if ((t/=1/2)==2) return 1;  if (!p) p=1*(.3*1.5);
-			if (a < Math.abs(1)) { a=1; var s=p/4; }
-			else var s = p/(2*Math.PI) * Math.asin (1/a);
-			if (t < 1) return -.5*(a*Math.pow(2,10*(t-=1)) * Math.sin( (t*1-s)*(2*Math.PI)/p ));
-			return a*Math.pow(2,-10*(t-=1)) * Math.sin( (t*1-s)*(2*Math.PI)/p )*.5 + 1;
-		},
-		easeInBack: function (t) {
-			var s = 1.70158;
-			return 1*(t/=1)*t*((s+1)*t - s);
-		},
-		easeOutBack: function (t) {
-			var s = 1.70158;
-			return 1*((t=t/1-1)*t*((s+1)*t + s) + 1);
-		},
-		easeInOutBack: function (t) {
-			var s = 1.70158; 
-			if ((t/=1/2) < 1) return 1/2*(t*t*(((s*=(1.525))+1)*t - s));
-			return 1/2*((t-=2)*t*(((s*=(1.525))+1)*t + s) + 2);
-		},
-		easeInBounce: function (t) {
-			return 1 - animationOptions.easeOutBounce (1-t);
-		},
-		easeOutBounce: function (t) {
-			if ((t/=1) < (1/2.75)) {
-				return 1*(7.5625*t*t);
-			} else if (t < (2/2.75)) {
-				return 1*(7.5625*(t-=(1.5/2.75))*t + .75);
-			} else if (t < (2.5/2.75)) {
-				return 1*(7.5625*(t-=(2.25/2.75))*t + .9375);
-			} else {
-				return 1*(7.5625*(t-=(2.625/2.75))*t + .984375);
-			}
-		},
-		easeInOutBounce: function (t) {
-			if (t < 1/2) return animationOptions.easeInBounce (t*2) * .5;
-			return animationOptions.easeOutBounce (t*2-1) * .5 + 1*.5;
-		}
-	};
-
 	//Variables global to the chart
 	var width = context.canvas.width;
 	var height = context.canvas.height;
@@ -160,6 +25,8 @@ window.Chart = function(context){
 		context.canvas.width = width * window.devicePixelRatio;
 		context.scale(window.devicePixelRatio, window.devicePixelRatio);
 	}
+	
+	//THESE TOP LEVEL FUNCTIONS SHOULD PROBABLY REMAIN UNCHANGED
 
 	this.PolarArea = function(data,options){
 	
@@ -349,14 +216,13 @@ window.Chart = function(context){
 		
 		return new Bar(data,config,context);		
 	}
-	
-	var clear = function(c){
-		c.clearRect(0, 0, width, height);
-	};
 
+	/*~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+	OBJECTS FOR DRAWING "CIRCULAR CHARTS" 
+	~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
+	
 	var PolarArea = function(data,config,ctx){
 		var maxSize, scaleHop, calculatedScale, labelHeight, scaleHeight, valueBounds, labelTemplateString;		
-		
 		
 		calculateDrawingSizes();
 		
@@ -785,6 +651,15 @@ window.Chart = function(context){
 		
 		
 	}
+
+
+	/*~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+	It's a personal preference, but I find drawing 
+	the "Cartesian" charts a little easier to grok. 
+	The circular charts are quite similar to each
+	other and could probably be DRYed out a little
+	bit, but these will require the most elbow grease.
+	~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
 
 	var Line = function(data,config,ctx){
 		var maxSize, scaleHop, calculatedScale, labelHeight, scaleHeight, valueBounds, labelTemplateString, valueHop,widestXLabel, xAxisLength,yAxisPosX,xAxisPosY, rotateLabels = 0;
@@ -1219,6 +1094,149 @@ window.Chart = function(context){
 	
 		}
 	}
+	
+	
+	/*~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+	DON'T TOUCH THIS CODE ...
+	but it would be great if someone could document it
+	~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
+	
+	var clear = function(c){
+		c.clearRect(0, 0, width, height);
+	};
+	
+	//Easing functions adapted from Robert Penner's easing equations
+	//http://www.robertpenner.com/easing/
+	var animationOptions = {
+		linear : function (t){
+			return t;
+		},
+		easeInQuad: function (t) {
+			return t*t;
+		},
+		easeOutQuad: function (t) {
+			return -1 *t*(t-2);
+		},
+		easeInOutQuad: function (t) {
+			if ((t/=1/2) < 1) return 1/2*t*t;
+			return -1/2 * ((--t)*(t-2) - 1);
+		},
+		easeInCubic: function (t) {
+			return t*t*t;
+		},
+		easeOutCubic: function (t) {
+			return 1*((t=t/1-1)*t*t + 1);
+		},
+		easeInOutCubic: function (t) {
+			if ((t/=1/2) < 1) return 1/2*t*t*t;
+			return 1/2*((t-=2)*t*t + 2);
+		},
+		easeInQuart: function (t) {
+			return t*t*t*t;
+		},
+		easeOutQuart: function (t) {
+			return -1 * ((t=t/1-1)*t*t*t - 1);
+		},
+		easeInOutQuart: function (t) {
+			if ((t/=1/2) < 1) return 1/2*t*t*t*t;
+			return -1/2 * ((t-=2)*t*t*t - 2);
+		},
+		easeInQuint: function (t) {
+			return 1*(t/=1)*t*t*t*t;
+		},
+		easeOutQuint: function (t) {
+			return 1*((t=t/1-1)*t*t*t*t + 1);
+		},
+		easeInOutQuint: function (t) {
+			if ((t/=1/2) < 1) return 1/2*t*t*t*t*t;
+			return 1/2*((t-=2)*t*t*t*t + 2);
+		},
+		easeInSine: function (t) {
+			return -1 * Math.cos(t/1 * (Math.PI/2)) + 1;
+		},
+		easeOutSine: function (t) {
+			return 1 * Math.sin(t/1 * (Math.PI/2));
+		},
+		easeInOutSine: function (t) {
+			return -1/2 * (Math.cos(Math.PI*t/1) - 1);
+		},
+		easeInExpo: function (t) {
+			return (t==0) ? 1 : 1 * Math.pow(2, 10 * (t/1 - 1));
+		},
+		easeOutExpo: function (t) {
+			return (t==1) ? 1 : 1 * (-Math.pow(2, -10 * t/1) + 1);
+		},
+		easeInOutExpo: function (t) {
+			if (t==0) return 0;
+			if (t==1) return 1;
+			if ((t/=1/2) < 1) return 1/2 * Math.pow(2, 10 * (t - 1));
+			return 1/2 * (-Math.pow(2, -10 * --t) + 2);
+			},
+		easeInCirc: function (t) {
+			if (t>=1) return t;
+			return -1 * (Math.sqrt(1 - (t/=1)*t) - 1);
+		},
+		easeOutCirc: function (t) {
+			return 1 * Math.sqrt(1 - (t=t/1-1)*t);
+		},
+		easeInOutCirc: function (t) {
+			if ((t/=1/2) < 1) return -1/2 * (Math.sqrt(1 - t*t) - 1);
+			return 1/2 * (Math.sqrt(1 - (t-=2)*t) + 1);
+		},
+		easeInElastic: function (t) {
+			var s=1.70158;var p=0;var a=1;
+			if (t==0) return 0;  if ((t/=1)==1) return 1;  if (!p) p=1*.3;
+			if (a < Math.abs(1)) { a=1; var s=p/4; }
+			else var s = p/(2*Math.PI) * Math.asin (1/a);
+			return -(a*Math.pow(2,10*(t-=1)) * Math.sin( (t*1-s)*(2*Math.PI)/p ));
+		},
+		easeOutElastic: function (t) {
+			var s=1.70158;var p=0;var a=1;
+			if (t==0) return 0;  if ((t/=1)==1) return 1;  if (!p) p=1*.3;
+			if (a < Math.abs(1)) { a=1; var s=p/4; }
+			else var s = p/(2*Math.PI) * Math.asin (1/a);
+			return a*Math.pow(2,-10*t) * Math.sin( (t*1-s)*(2*Math.PI)/p ) + 1;
+		},
+		easeInOutElastic: function (t) {
+			var s=1.70158;var p=0;var a=1;
+			if (t==0) return 0;  if ((t/=1/2)==2) return 1;  if (!p) p=1*(.3*1.5);
+			if (a < Math.abs(1)) { a=1; var s=p/4; }
+			else var s = p/(2*Math.PI) * Math.asin (1/a);
+			if (t < 1) return -.5*(a*Math.pow(2,10*(t-=1)) * Math.sin( (t*1-s)*(2*Math.PI)/p ));
+			return a*Math.pow(2,-10*(t-=1)) * Math.sin( (t*1-s)*(2*Math.PI)/p )*.5 + 1;
+		},
+		easeInBack: function (t) {
+			var s = 1.70158;
+			return 1*(t/=1)*t*((s+1)*t - s);
+		},
+		easeOutBack: function (t) {
+			var s = 1.70158;
+			return 1*((t=t/1-1)*t*((s+1)*t + s) + 1);
+		},
+		easeInOutBack: function (t) {
+			var s = 1.70158; 
+			if ((t/=1/2) < 1) return 1/2*(t*t*(((s*=(1.525))+1)*t - s));
+			return 1/2*((t-=2)*t*(((s*=(1.525))+1)*t + s) + 2);
+		},
+		easeInBounce: function (t) {
+			return 1 - animationOptions.easeOutBounce (1-t);
+		},
+		easeOutBounce: function (t) {
+			if ((t/=1) < (1/2.75)) {
+				return 1*(7.5625*t*t);
+			} else if (t < (2/2.75)) {
+				return 1*(7.5625*(t-=(1.5/2.75))*t + .75);
+			} else if (t < (2.5/2.75)) {
+				return 1*(7.5625*(t-=(2.25/2.75))*t + .9375);
+			} else {
+				return 1*(7.5625*(t-=(2.625/2.75))*t + .984375);
+			}
+		},
+		easeInOutBounce: function (t) {
+			if (t < 1/2) return animationOptions.easeInBounce (t*2) * .5;
+			return animationOptions.easeOutBounce (t*2-1) * .5 + 1*.5;
+		}
+	};
 	
 	function calculateOffset(val,calculatedScale,scaleHop){
 		var outerValue = calculatedScale.steps * calculatedScale.stepValue;
